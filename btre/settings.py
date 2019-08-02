2a8fe6b683dbfe7656628a3be3c0416874f210aa
JainSamyak8853 <hackmykaamprivate@gmail.com>
2019-08-02 17:15:48 +0530

    final commit

--- btre/settings.py
+++ btre/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x+h)j%i$7^8gp+lj0%#9upu7gcx)r^7!_j_9ny(+=p77hggp21'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-realestate-app.herokuapp.com']
 
